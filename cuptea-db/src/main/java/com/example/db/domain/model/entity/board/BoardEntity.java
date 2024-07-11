41eae2503dc27065bbf57c9125e4d007a4f02ef1
juhoon212 <ws5501@naver.com>
2024-07-11 16:01:39 +0900

    feat: BoardEntity

---
+++ cuptea-db/src/main/java/com/example/db/domain/model/entity/board/BoardEntity.java
@@ -0,0 +1,45 @@
+package com.example.db.domain.model.entity.board;
+
+import com.example.db.common.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.apache.commons.lang3.ObjectUtils;
+
+import java.util.UUID;
+
+@Entity
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Getter
+@EqualsAndHashCode(of = "boardId", callSuper = true)
+@ToString(callSuper = true)
+@Table(name = "Board")
+/**
+ * 방명록
+ */
+public class BoardEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID boardId;
+
+    @Column(name = "member_id")
+    private UUID memberId;
+
+    @Lob
+    private String text;
+
+    @Column(name = "thumbnail_url")
+    private String thumbnailUrl;
+
+    @Column(name = "use_yn", length = 1)
+    private String useYn;
+
+    @PrePersist
+    public void prePersist() {
+        useYn = ObjectUtils.defaultIfNull(useYn, "Y");
+    }
+}
