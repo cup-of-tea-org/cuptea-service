ee7bd2b792fdc4074833428d5bcd52251d0b9a3a
juhoon212 <ws5501@naver.com>
2024-07-28 21:56:43 +0900

    feat: Board cu API && Board Service

---
+++ cuptea-db/src/main/java/com/example/db/repository/querydsl/CustomBoardRepository.java
@@ -0,0 +1,6 @@
+package com.example.db.repository.querydsl;
+
+public interface CustomBoardRepository {
+
+//    List<SearchBoardResponseDto> searchBoard(SearchBoardRequestDto searchBoardRequestDto);
+}
