25d5f97ce3c0e5bb80c88ece2658c41dead00dfe
juhoon212 <ws5501@naver.com>
2024-07-28 23:25:16 +0900

    s3

---
+++ cuptea-api/src/main/java/com/example/cupteaapi/config/S3Config.java
@@ -0,0 +1,9 @@
+package com.example.cupteaapi.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.example.db")
+public class S3Config {
+}
